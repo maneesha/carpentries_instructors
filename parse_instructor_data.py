ebe89c85dc8ef81122503527ec9aac26303fa8af
maneesha sane <maneeshasane@gmail.com>
2017-10-26 18:37:58 -0400

    change url to badged instructors only

--- parse_instructor_data.py
+++ parse_instructor_data.py
@@ -1,6 +1,7 @@
 import sys
 import requests
 from requests.auth import HTTPBasicAuth
+import json
 
 try:
     import local_settings
@@ -9,7 +10,9 @@ except:
     print("Local settings not found")
     sys.exit(1)
 
-data = requests.get('https://amy.software-carpentry.org/api/v1/persons/?format=json', auth=HTTPBasicAuth(local_settings.user, local_settings.pw))
-
+data = requests.get('https://amy.software-carpentry.org/api/v1/persons/?badges=2&badges=5&username=&personal=&middle=&family=&email=&may_contact=1&is_instructor=1&o=lastname', auth=HTTPBasicAuth(local_settings.user, local_settings.pw))
 
 print(data.text)
+
+
+

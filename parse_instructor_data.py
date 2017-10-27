e9d2ca8a6932865f279c8952a52f343bf7f936ac
maneesha sane <maneeshasane@gmail.com>
2017-10-27 09:56:16 -0400

    add badges to instructor dict

--- parse_instructor_data.py
+++ parse_instructor_data.py
@@ -33,10 +33,19 @@ for person in persons:
     for workshop in workshops:
         workshops_list.append(workshop['event'].split("/")[-2] + " " + workshop['role'])
 
+
+    badges = requests.get(person['awards'], auth=HTTPBasicAuth(local_settings.user, local_settings.pw))
+    badges = json.loads(badges.text)
+    badges_list = []
+    for badge in badges:
+        # print(badge['badge'], badge['awarded'])
+        badges_list.append(badge['badge'] + ' awarded on ' + badge['awarded'])
+
     d['full name'] = person['personal'] + " " +  person['family']
     d['email'] = person['email']
     d['airport'] = airport
     d['workshops'] = workshops_list
+    d['badges'] = badges_list
 
     instructors.append(d)
 

60973716b1decde6f2d6b80c49324e34439eacd9
maneesha sane <maneeshasane@gmail.com>
2017-10-27 11:39:41 -0400

    move data assignment to email_template file

---
+++ email_template.py
@@ -0,0 +1,5 @@
+import parse_instructor_data
+
+persons_data = parse_instructor_data.requests.get('https://amy.software-carpentry.org/api/v1/persons/?badges=2&badges=5&username=&personal=&middle=&family=&email=&may_contact=1&is_instructor=1&o=lastname', auth=parse_instructor_data.HTTPBasicAuth(parse_instructor_data.local_settings.user, parse_instructor_data.local_settings.pw))
+
+parse_instructor_data.create_instructor_dictionary(persons_data)
--- parse_instructor_data.py
+++ parse_instructor_data.py
@@ -10,11 +10,6 @@ except:
     print("Local settings not found")
     sys.exit(1)
 
-data = requests.get('https://amy.software-carpentry.org/api/v1/persons/?badges=2&badges=5&username=&personal=&middle=&family=&email=&may_contact=1&is_instructor=1&o=lastname', auth=HTTPBasicAuth(local_settings.user, local_settings.pw))
-
-# print(data.text)
-
-
 
 def create_instructor_dictionary(data):
 # Pull just the persons records & transform to python dictionary

0c8e96836001f55350505b034270a2425f7d23ab
Jonah Duckles <jonah@duckles.org>
2017-10-31 13:14:59 +1300

    Wired for email sending

--- send_email.py
+++ send_email.py
@@ -1,5 +1,6 @@
 import parse_instructor_data
 import jinja2
+import send_util
 
 url = 'https://amy.software-carpentry.org/api/v1/persons/?badges=2&badges=5&is_instructor=1&may_contact=1&o=lastname&page=1'
 
@@ -13,7 +14,7 @@ with open("email.txt") as fp:
 template = jinja2.Template(email_template)
 
 for instructor in instructors:
-    
     email_body = template.render(name=instructor['full name'], airport=instructor['airport'], workshops=instructor['workshops'] )
 
+    #send_util.send_email(instructor['email'],'Please update your Software/Data Carpentry Instructor Information', email_body)
     print(email_body)
---
+++ send_util.py
@@ -0,0 +1,29 @@
+import mimetypes
+import smtplib
+import getpass
+import email
+import email.utils
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import local_settings
+
+# Use Mailgun credentials-
+smtp_user = local_settings.smtp_user
+smtp_passwd = local_settings.smtp_passwd
+
+server = smtplib.SMTP('smtp.mailgun.org:587')
+server.starttls()
+server.login(smtp_user, smtp_passwd)
+
+def send_email(email_to,email_subject,email_body,email_from="the Carpentries Team <team@carpentries.org>"):
+    """
+    Using a pre-rendered body, email
+    """
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = email_subject
+    msg['From'] = email_from
+    msg['To'] = email_to
+    msg.attach(MIMEText(email_body, 'plain'))
+    to_address = email_to
+    from_address = email_from
+    server.sendmail(from_address, to_address, msg.as_string())

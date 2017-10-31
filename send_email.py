ac45cc5999e9968429139254c254eeaf3f4e6160
maneesha sane <maneeshasane@gmail.com>
2017-10-31 14:45:13 -0400

    separate out page num, preparing to loop through all pages

--- send_email.py
+++ send_email.py
@@ -1,7 +1,11 @@
 import parse_instructor_data
 import jinja2
 
-url = 'https://amy.software-carpentry.org/api/v1/persons/?badges=2&badges=5&is_instructor=1&may_contact=1&o=lastname&page=1'
+# This whole thing needs to be in a loop to go through all 12 pages of API
+
+page_number = 1
+
+url = 'https://amy.software-carpentry.org/api/v1/persons/?badges=2&badges=5&is_instructor=1&may_contact=1&o=lastname&page=' + str(page_number)
 
 persons_data = parse_instructor_data.requests.get(url, auth=parse_instructor_data.HTTPBasicAuth(parse_instructor_data.local_settings.user, parse_instructor_data.local_settings.pw))
 
@@ -17,4 +21,3 @@ for instructor in instructors:
     email_body = template.render(name=instructor['full name'], airport=instructor['airport'], workshops=instructor['workshops'], badges=instructor['badges'] )
 
     print(email_body)
-    
